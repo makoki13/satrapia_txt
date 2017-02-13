3613e2518d15e11d73cdb8ab338be4643176c490
makoki13 <pablo.makoki@gmail.com>
2017-02-13 21:54:39 +0100

    Inicio

---
+++ Satrapia_txt/pantalla_principal.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "curses.h"
+
+void pantallaPrincipal() {
+	mvprintw(5, 10, "1.- Cámara real");
+	mvprintw(6, 10, "2.- Cámara de ministros");
+	mvprintw(7, 10, "1.- Cámara real");
+}
